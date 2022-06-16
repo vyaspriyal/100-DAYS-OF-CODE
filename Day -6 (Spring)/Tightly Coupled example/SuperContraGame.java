e4a451dabf8cd2de7ff52022693875b7f6e150f6
vyaspriyal <65209828+vyaspriyal@users.noreply.github.com>
2022-06-16 13:24:47 +0530

    Create SuperContraGame.java

---
+++ Day -6 (Spring)/Tightly Coupled example/SuperContraGame.java
@@ -0,0 +1,21 @@
+package com.example.last.game;
+
+public class SuperContraGame {
+	
+		public void  up() {
+			System.out.println("up");
+			
+		}
+		public void down() {
+			System.out.println("down");
+		}
+		public void right() {
+			System.out.println("right;");
+		}
+		public void left() {
+			System.out.println("left");
+	
+
+}
+
+}
