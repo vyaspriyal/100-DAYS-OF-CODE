53b6ac73e1fd2a027eb7b0a4aff5f4339979577f
vyaspriyal <65209828+vyaspriyal@users.noreply.github.com>
2022-06-16 13:23:50 +0530

    Create LastApplication.java

---
+++ Day -6 (Spring)/Tightly Coupled example/LastApplication.java
@@ -0,0 +1,26 @@
+package com.example.last;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.last.game.GameRunner;
+import com.example.last.game.MarioGame;
+import com.example.last.game.SuperContraGame;
+
+@SpringBootApplication
+public class LastApplication {
+
+	public static void main(String[] args) {
+//	MarioGame game = new MarioGame();
+SuperContraGame game = new SuperContraGame();	
+//	SpringApplication.run(LastApplication.class, args);
+	GameRunner runner = new GameRunner(game);
+
+	runner.runGame();
+	
+	
+	
+	
+	}
+
+}
