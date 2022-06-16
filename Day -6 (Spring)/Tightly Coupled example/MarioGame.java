63639732d0053872fb3aa5b84ba11bb515e2a18f
vyaspriyal <65209828+vyaspriyal@users.noreply.github.com>
2022-06-16 13:24:17 +0530

    Create  MarioGame.java

---
+++ Day -6 (Spring)/Tightly Coupled example/MarioGame.java
@@ -0,0 +1,18 @@
+package com.example.last.game;
+
+public class MarioGame {
+	public void  up() {
+		System.out.println("jump");
+		
+	}
+	public void down() {
+		System.out.println("go in the hole");
+	}
+	public void right() {
+		System.out.println("stop");
+	}
+	public void left() {
+		System.out.println("go faster");
+	}
+
+}
