cd5a1cdbd91ebdf42cffd64807a9fbd51a5a6bdf
Priyal Vyas <65209828+vyaspriyal@users.noreply.github.com>
2022-06-17 18:28:45 +0530

    Add files via upload

---
+++ Day - 7 (Spring Boot)/CourseController.java
@@ -0,0 +1,21 @@
+package com.example.springboot.courses.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springboot.courses.bean.Course;
+
+
+
+@RestController 
+public class CourseController {
+	@GetMapping("/courses")
+	public List<Course> getAllCourses(){
+		return Arrays.asList(new Course(1,"spring","priyal"));
+	}
+	
+
+}
